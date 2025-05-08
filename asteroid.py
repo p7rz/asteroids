7fbae9cac795fcae19437175439e06d13f6645aa
p7rz <taavipeeters@gmail.com>
2025-05-08 18:20:51 +0300

    before adding substance to asteroids

---
+++ asteroid.py
@@ -0,0 +1,9 @@
+from circleshape import *
+from constants import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+    
+    def draw():
+        pygame.draw.circle
--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
+
+    print("Starting Asteroids!")
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
+
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
+    print(pygame.sprite.Group.sprites(drawable))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, color = 'black')
+        
+        updatable.update(dt)
+        
+        screen.fill(color = 'black')
+
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
     
-    print("Starting Asteroids!")
-    print(f'Screen width: {SCREEN_WIDTH}')
-    print(f'Screen height: {SCREEN_HEIGHT}')
 
 if __name__ == "__main__":
     main()
